d90c4f872d9de0f95a7e9a95869f25c90f10557f
CHAORAWIT JAMNONG <6131305009@lamduan.mfu.ac.th>
2021-03-26 04:23:45 +0800

    fix

--- components/_App/View/MainView.js
+++ components/_App/View/MainView.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import axios from 'axios';
-import {Item, Label,Grid,Image} from 'semantic-ui-react'
-import ProductList from '../../Index/ProductList';
-function MainView({products}){
+import {Image} from 'semantic-ui-react'
+
+function MainView(){
     return<>
       <Image stype ={{width: '2500',height: '1000'}} 
       src='/static/favicon/Banner_ASEAN_TOP10-01.png'   />
    
     </>
 }
-MainView.getInitialProps = async () =>{
-    const url ='http://localhost:3000/api/products'
-    const reponse = await axios.get(url)
-    return {products: reponse.data};
-    //fetch data on server 
-    //return reponse data aas an object
-    // note :this object will be  merged with existing props
-  }
   export default MainView;
