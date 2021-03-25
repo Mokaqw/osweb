9ea2f9fe74261d0491fdcafbb14824d4aad2baf8
CHAORAWIT JAMNONG <6131305009@lamduan.mfu.ac.th>
2021-03-26 04:24:52 +0800

    Delete function

--- components/Product/ProductSummary.js
+++ components/Product/ProductSummary.js
@@ -1,7 +1,19 @@
-import {Header,Button, Item, Label } from "semantic-ui-react";
+import React from 'react'
+import {Header,Button, Item, Label,Modal } from "semantic-ui-react";
+import axios from 'axios';
+import {useRouter} from 'next/router';
 import AddProductToCart from "./AddProductToCart";
 import AddProductwishlist from './AddProductwishlist'
 function ProductSummary({ name, exampleUrl, _id, price, school_of,description }) {
+  const [modal, setModal] = React.useState(false);
+  const router = useRouter();
+
+  async function handleDelete() {
+    const url = 'http://localhost:3000/api/product';
+    const payload = { params: { _id } };
+    await axios.delete(url, payload);
+    router.push("/buy");
+ }
   return (
     <Item.Group>
       <Item>
@@ -19,11 +31,28 @@ function ProductSummary({ name, exampleUrl, _id, price, school_of,description })
           <Header text style={{ paddingTop: "2em" }}as="h3">About this product</Header>
           
       <p>{description}</p>
-         <Button
-       // icon="trash alternate outline"
-       // color="red"
-       // content="Delete Product"
+      <br/>
+         <Button icon="trash alternate outline"  color="red"    content="Delete Product"
+         onClick={()=>setModal(true)}
       /> 
+      <Modal open={modal} dimmer="blurring">
+        <Modal.Header>Confirm Delete</Modal.Header>
+        <Modal.Content>
+          <p>Are yoy sure you want to delete this  product</p>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button onClick={()=> setModal(false)} content="Cancel"/>
+          <Button
+            negative
+            icon="trash"
+            labelPosition="right"
+            content="Delete"
+            onClick={handleDelete}
+          />
+
+        </Modal.Actions>
+      </Modal>
+      
         </Item.Content>
       </Item>
     </Item.Group>
