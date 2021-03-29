3f915a47fee4b04d03ee14064b0504b18df5626d
CHAORAWIT JAMNONG <6131305009@lamduan.mfu.ac.th>
2021-03-29 16:52:54 +0800

    insert cloudinary url

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 module.exports = {
   env: {
     MONGO_SRV: "mongodb+srv://mosscj4582:moss4582@onesummary.u68g1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
-    CLOUDINARY_URL: "<insert-cloudinary-url>",
-    STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
+    JWT_SECRET: "",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/moss4582/image/upload",
+    STRIPE_SECRET_KEY: ""
   }
 };
